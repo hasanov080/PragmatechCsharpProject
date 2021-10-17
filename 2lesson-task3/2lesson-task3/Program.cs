4a0d0f3070c64f7da0f30033f69a52fe0280ac04
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 22:38:17 +0400

    Add files via upload

---
+++ 2lesson-task3/2lesson-task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2lesson_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sample = Console.ReadLine();
+            Console.WriteLine(dontRepeat(sample));
+        }
+        static string dontRepeat(string provided)
+        {
+            string withoutRepeat = "";
+            int check = 0;
+            for(int i = 0; i< provided.Length; i++)
+            {
+                check = 0;
+                for(int j = i-1; j>=0; j--)
+                {
+                    if(provided[j]==provided[i])
+                    {
+                        check = 1;
+                        break;
+                    }
+                }
+                if(check == 0)
+                {
+                    withoutRepeat += provided[i];
+                }
+            }
+            return withoutRepeat;
+        }
+    }
+}
