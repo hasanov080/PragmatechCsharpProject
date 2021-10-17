e1a6e70f32cc1436d2709fdcfcf88f4c806ef1c8
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 22:37:16 +0400

    Add files via upload
    1 task

---
+++ 1st-task/1st-task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1st_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, sum=0;
+            for(int i = 0; i < 5; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                sum += x;
+            }
+            Console.WriteLine(sum / 5);
+
+        }
+    }
+}
