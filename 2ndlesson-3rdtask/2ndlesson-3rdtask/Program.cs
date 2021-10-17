8be565e883f5e35eb17473e66c7aee60cd387b7e
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 23:05:27 +0400

    Add files via upload

---
+++ 2ndlesson-3rdtask/2ndlesson-3rdtask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _2ndlesson_3rdtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string polindrom = Console.ReadLine();
+            Console.WriteLine(isPolindrom(polindrom));
+        }
+        static bool isPolindrom(string check)
+        {
+            if (ReverseString(check) == check)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        static string ReverseString(string provided)
+        {
+            string reverse = "";
+            for (int i = provided.Length - 1; i >= 0; i--)
+            {
+                reverse += provided[i];
+            }
+            return reverse;
+        }
+    }
+}
