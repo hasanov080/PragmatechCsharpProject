ff0b2eee61139ac41da40447064f6fa0aab04a9c
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 22:53:19 +0400

    Add files via upload

---
+++ 5th-task/5th-task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5th_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()), mult=1, sum =0;
+            for(int i = 1; i<n; i++)
+            {
+                mult *= i;
+                sum += i;
+            }
+            Console.WriteLine(mult - sum);
+        }
+    }
+}
