6a1aa50dd367e9ef5978564487f44fd76919f039
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 23:07:36 +0400

    Add files via upload

---
+++ 2ndLesson-task1/2ndLesson-task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2ndLesson_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int beginPoint = Convert.ToInt32(Console.ReadLine()), stringLength;
+            try {
+                stringLength = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch {
+                stringLength = s.Length-beginPoint;
+            }
+
+            
+            Console.WriteLine(SubString(s, beginPoint, stringLength));
+        }
+        static string SubString(string mainWord, int beginPoint, int stringLength)
+        {
+            string sub="";
+            if(stringLength == null)
+            {
+                for(int i = beginPoint; i<mainWord.Length; ++i)
+                {
+                    sub += mainWord[i];
+                }
+                return sub;
+            }
+            else
+            {
+                for (int i = beginPoint; i < beginPoint + stringLength; ++i)
+                {
+                    sub += mainWord[i];
+                }
+                return sub;
+            };
+
+        }
+
+    }
+}
