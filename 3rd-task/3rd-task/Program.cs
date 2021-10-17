584ebe009871f5f70ff2855ff7b19d806f259b02
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 22:58:00 +0400

    Add files via upload

---
+++ 3rd-task/3rd-task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3rd_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<=10; ++i)
+            {
+                Console.WriteLine("{0}*{1}={2}", a, i, a * i);
+            }
+
+        }
+    }
+}
