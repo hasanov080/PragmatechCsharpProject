886240ef29aac80aa931f188ffdf9f0796f6d5b4
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 23:04:34 +0400

    Add files via upload

---
+++ 2ndlesson-2ndtask/2ndlesson-2ndtask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2ndlesson_2ndtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string provided = Console.ReadLine();
+            Console.WriteLine(ReverseString(provided));
+        }
+        static string ReverseString(string provided)
+        {
+            string reverse = "";
+            for(int i = provided.Length - 1; i>=0; i--)
+            {
+                reverse += provided[i];
+            }
+            return reverse;
+        }
+    }
+}
