388edec76298a348f685b26d25fa6d19df542ac2
hasanov080 <88917631+hasanov080@users.noreply.github.com>
2021-10-17 23:02:52 +0400

    Add files via upload

---
+++ 2nd-task/2nd-task/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2nd_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine()), b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} {1}", a * b, 2*(a + b)) ;
+        }
+    }
+}
